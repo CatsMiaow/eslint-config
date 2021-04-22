f19ccbb4919ab6fc612e6fea748482b4aea9ef41
CatsMiaow <infiltrator@naver.com>
2021-04-22 16:00:42 +0900

    Feat @typescript-eslint/all

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ module.exports = {
     "plugin:jest/recommended",
     "plugin:unicorn/recommended",
     "plugin:sonarjs/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/all",
   ],
   rules: {
     "class-methods-use-this": "off",
@@ -36,15 +35,6 @@ module.exports = {
     "object-curly-newline": "off",
     "spaced-comment": ["error", "always", { line: { markers: ["/", "#region", "#endregion"] } }],
 
-    // Change eslint rule to @typescript-eslint rule
-    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
-    "no-dupe-class-members": "off",
-    "no-duplicate-imports": "off",
-    "no-loop-func": "off",
-    "no-return-await": "off",
-    "no-unused-expressions": "off",
-    "object-curly-spacing": "off",
-
     // https://github.com/benmosher/eslint-plugin-import/issues/1753
     "import/named": "off",
     // https://github.com/benmosher/eslint-plugin-import/issues/1453
@@ -53,15 +43,14 @@ module.exports = {
     "import/order": ["error", { groups: [["builtin", "external", "internal"]], "newlines-between": "always", alphabetize: { order: "asc", caseInsensitive: true } }],
     "import/prefer-default-export": "off",
 
-    "@typescript-eslint/consistent-indexed-object-style": "error",
+    // Change eslint rule to @typescript-eslint rule
+    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+
+    //#region typescript-eslint
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/all.ts
     "@typescript-eslint/consistent-type-assertions": ["error", { assertionStyle: "angle-bracket" }],
     "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
-    "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/explicit-member-accessibility": ["error", { overrides: { constructors: "no-public" } }],
-    // https://github.com/typescript-eslint/typescript-eslint/issues/977
-    "@typescript-eslint/lines-between-class-members": "off",
-    "@typescript-eslint/member-delimiter-style": "error",
-    "@typescript-eslint/member-ordering": "error",
     "@typescript-eslint/naming-convention": ["error",
       { selector: "default", format: ["strictCamelCase"] },
       { selector: "variable", format: ["strictCamelCase", "UPPER_CASE", "StrictPascalCase"] },
@@ -71,27 +60,19 @@ module.exports = {
       { selector: "typeProperty", format: null },
       { selector: "typeLike", format: ["StrictPascalCase"] },
       { selector: "enumMember", format: ["UPPER_CASE"] }],
-    "@typescript-eslint/no-dupe-class-members": "error",
-    "@typescript-eslint/no-duplicate-imports": "error",
     "@typescript-eslint/no-floating-promises": ["error", { ignoreIIFE: true, ignoreVoid: true }],
     "@typescript-eslint/no-inferrable-types": ["error", { ignoreParameters: true, ignoreProperties: true }],
-    "@typescript-eslint/no-loop-func": "error",
     "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-    "@typescript-eslint/no-unnecessary-qualifier": "error",
-    "@typescript-eslint/no-unnecessary-type-arguments": "error",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/no-unnecessary-type-constraint": "error",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unused-expressions": "error",
     "@typescript-eslint/object-curly-spacing": ["error", "always"],
-    "@typescript-eslint/prefer-includes": "off",
-    "@typescript-eslint/prefer-optional-chain": "error",
-    "@typescript-eslint/promise-function-async": "error",
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/return-await": "error",
     "@typescript-eslint/typedef": ["error", { arrowParameter: true, memberVariableDeclaration: true, parameter: true, propertyDeclaration: true }],
     "@typescript-eslint/unbound-method": ["error", { ignoreStatic: true }],
+
+    // https://github.com/typescript-eslint/typescript-eslint/issues/977
+    "@typescript-eslint/lines-between-class-members": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/prefer-includes": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    //#endregion
 
     "sonarjs/no-duplicate-string": "off",
 
