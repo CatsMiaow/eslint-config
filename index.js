fc64c854d358c4d94b00bc7344e9eb530bbd2331
CatsMiaow <infiltrator@naver.com>
2022-06-20 14:23:39 +0900

    Add parameter-properties off

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = {
     "@typescript-eslint/no-inferrable-types": ["error", { ignoreParameters: true, ignoreProperties: true }],
     "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
     "@typescript-eslint/object-curly-spacing": ["error", "always"],
+    "@typescript-eslint/parameter-properties": "off",
     "@typescript-eslint/typedef": ["error", { arrowParameter: true, memberVariableDeclaration: true, parameter: true, propertyDeclaration: true }],
     "@typescript-eslint/unbound-method": ["error", { ignoreStatic: true }],
 
